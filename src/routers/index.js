af33459ac6b9a3407f7dbbe14b339b0730a524ea
tharindi22 <ttharindiattanayaka@gmail.com>
2024-12-29 18:03:23 +0530

    New Updates

---
+++ src/routers/index.js
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <App/>,
+        children : [
+            {
+               path : "",
+               element : <Home/>
+            }
+        ]
+    }
+])
