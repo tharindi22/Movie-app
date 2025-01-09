538127959d7275fcdb35b7e7cc8991b6be5165ae
tharindi22 <ttharindiattanayaka@gmail.com>
2025-01-08 16:50:19 -0800

    Remake

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import './App.css'; // Make sure this file exists
+import Header from './components/Header';
 
 function App() {
   return (
     <main>
+      <Header />
       <Outlet /> 
+      <footer />
     </main>
   );
 }
