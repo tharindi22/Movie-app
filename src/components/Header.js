a75f814600b9f588f9aa9b2db13b082d2bc2fb7b
tharindi22 <ttharindiattanayaka@gmail.com>
2025-01-09 21:17:28 -0800

    Creating Header for Movie App

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const Header = () => {
     return(
-        <div>
-            Header
-        </div>
+        <header className="flex top-0 w-full h-16 bg-neutral-600 bg-opacity-75">
+
+        </header>
+
     )
 }
 
