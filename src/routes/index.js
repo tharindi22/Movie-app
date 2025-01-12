d99d7b47420efdc60c22b6c300db81b06c48bece
tharindi22 <ttharindiattanayaka@gmail.com>
2025-01-12 21:37:30 +0530

    Error Corrections

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App.jsx';
+import Home from '../pages/Home.jsx'; // Ensure Home is properly exported in its file
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        { path: "", 
-            element: <Home /> },
-       
-      ],
-    },
-  ]);
-  
-export default router
+  {
+    path: "/",
+    element:  <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />, // Maps to the Home component
+      },
+    ],
+  },
+]);
+
+export default router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this path according to your project structure
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
