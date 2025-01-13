348644e18f450f5abff91d43972efe54b79d50e6
tharindi22 <ttharindiattanayaka@gmail.com>
2025-01-13 22:34:10 +0530

    Completing the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="main lg:flex md:flex flex-wrap justify-between items-center px-6 bg-[#40407a] py-2 h-17 shadow-md sticky top-0">
+      <div className="left flex items-center space-x-3 justify-center">
+        
+        <img className="w-14  object-contain " src="4.png" alt="img" />
+        <h2 className="text-white font-bold text-2xl">Movie Masala</h2>
+      </div>
+      <div className="right">
+        <ul className="flex space-x-5 text-white font-medium justify-center px-5">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Service</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
